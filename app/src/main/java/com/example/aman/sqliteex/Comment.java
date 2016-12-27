d4ff2ebde07c63e4749dc2734ac15c2112b07bb3
Aman Pradhan <aprdhn.30000@gail.com>
2016-12-27 14:09:14 +0530

    Initial commit

---
+++ app/src/main/java/com/example/aman/sqliteex/Comment.java
@@ -0,0 +1,32 @@
+package com.example.aman.sqliteex;
+
+/**
+ * Created by aman on 26/12/16.
+ */
+
+public class Comment {
+    private long id;
+    private String comment;
+
+    public long getId()
+    {
+        return id;
+    }
+    public void setId(long id)
+    {
+        this.id = id;
+    }
+
+    public String getComment()
+    {
+        return comment;
+    }
+    public String toString()
+    {
+        return comment;
+    }
+    public void setComment(String comment)
+    {
+        this.comment = comment;
+    }
+}
